dc3d74ba5c9b6da1be660a44ba84b6cef908933e
Andrew Piispanen <apiispanen1@babson.edu>
2021-03-16 15:41:38 -0400

    updated tables

---
+++ Queries/Employee_Challenge_starter_code.sql
@@ -0,0 +1,14 @@
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON () _____,
+______,
+______,
+______
+
+INTO fifties_employees
+FROM retirement_info
+ORDER BY _____, _____ DESC;
+SELECT * FROM dept_manager
+
+
+SELECT * FROM retirement_info
+JOIN employees ON retirement_info.emp_no = employees.emp_no;
--- Queries/schema.sql
+++ Queries/schema.sql
@@ -5,8 +5,9 @@ CREATE TABLE departments (
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
-CREATE TABLE employees (
-     emp_no INT NOT NULL,
+
+CREATE TABLE employees ( 
+	 emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -25,9 +26,6 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
@@ -36,3 +34,83 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+
+DROP TABLE "dep_emp"
+
+CREATE TABLE "dep_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL,
+    FOREIGN KEY("dept_no") REFERENCES "departments" ("dept_no"),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (dept_no,emp_no)
+);
+
+CREATE TABLE "titles" (
+    "emp_no" int   NOT NULL,
+    "title" varchar   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  	PRIMARY KEY (emp_no)
+);
+
+
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.gender,
+    s.salary,
+    s.to_date
+	INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+
+SELECT * FROM employees
+
+SELECT e.*,
+de.dept_no,
+d.dept_name
+INTO current_emp
+FROM employees AS e
+	INNER JOIN dep_emp AS de
+	ON (de.emp_no = e.emp_no)
+	INNER JOIN departments AS d
+	ON (d.dept_no = de.dept_no) ;
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+		
+		
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name		
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dep_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
