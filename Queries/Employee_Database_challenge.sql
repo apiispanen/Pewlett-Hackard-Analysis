8c64060bf7cb042807f23e14d5bcc0745e7e13ff
Andrew Piispanen <apiispanen1@babson.edu>
2021-03-16 16:34:41 -0400

    README edits

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,5 @@
+-- DELIVERABLE 1
+
 -- MAKE THE RETIREMENT TITLES TABLE
 SELECT
 e.emp_no,
@@ -33,3 +35,17 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+-- DELIVERABLE 2
+
+-- MENTORSHIP ELIGIBILITY
+SELECT DISTINCT ON (e.emp_no) 
+e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibility
+FROM employees AS e
+JOIN dep_emp AS de ON e.emp_no = de.emp_no
+JOIN titles AS t ON t.emp_no = e.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND t.to_date = '9999-01-01'
+ORDER BY e.emp_no ASC, t.to_date ASC;
+
